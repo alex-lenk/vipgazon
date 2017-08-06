694afdfaddfbc52ae18c1a79f5daddc14ab1e21a
artlenk.ru@gmail.com <df5ukoi7ht>
2017-08-06 07:49:32 +0300

    Fixed slider, button to safari, font-size to media query

--- src/js/common.js
+++ src/js/common.js
@@ -7,31 +7,7 @@ $(document).ready(function () {
     );
 
     $('.slider').slick({
-        prevArrow: '<div data-role="none" class="slick-prev" aria-label="Previous" tabindex="0"><span><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20"><path d="M13.89 17.418c.27.272.27.71 0 .98s-.7.27-.968 0l-7.83-7.91c-.268-.27-.268-.706 0-.978l7.83-7.908c.268-.27.7-.27.97 0s.267.71 0 .98L6.75 10l7.14 7.418z"/></svg></span></div>',
-        nextArrow: '<div data-role="none" class="slick-next" aria-label="Next" tabindex="0"><span><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20"><path d="M13.25 10L6.11 2.58c-.27-.27-.27-.707 0-.98.267-.27.7-.27.968 0l7.83 7.91c.268.27.268.708 0 .978l-7.83 7.908c-.268.27-.7.27-.97 0s-.267-.707 0-.98L13.25 10z"/></svg></span></div>'
+        prevArrow: '<div data-role="none" class="slick-prev" aria-label="Previous" tabindex="0"><span><svg class="arrow"><use xlink:href="img/sprite.svg#arrow"></use></svg></span></div>',
+        nextArrow: '<div data-role="none" class="slick-next" aria-label="Next" tabindex="0"><span><svg class="arrow"><use xlink:href="img/sprite.svg#arrow"></use></svg></span></div>'
     });
 });
-
-/*
-
-
- $('.slider-container').slick({
- dots: true
- });
-
- $(".filter-toggle").click(
- function () {
- $('#mse2_filters').toggleClass("open-filter");
- }
- );
-
- $('.img-zoom').zoom({
- magnify: 0.5
- });
-
-
- //= init/fixednav.js
-
- //= init/selectCustom.js
-
- */
