2530ac09b10c81bcffee177b218f2460562006e8
artlenk.ru@gmail.com <df5ukoi7ht>
2017-08-18 21:01:25 +0300

    Created autoplay for slider

--- src/js/common.js
+++ src/js/common.js
@@ -7,8 +7,10 @@ $(document).ready(function () {
     );
 
     $('.slider').slick({
-        prevArrow: '<div data-role="none" class="slick-prev" aria-label="Previous" tabindex="0"><span><svg class="arrow"><use xlink:href="img/sprite.svg#arrow"></use></svg></span></div>',
-        nextArrow: '<div data-role="none" class="slick-next" aria-label="Next" tabindex="0"><span><svg class="arrow"><use xlink:href="img/sprite.svg#arrow"></use></svg></span></div>'
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: '<div data-role="none" class="slick-prev" aria-label="Previous" tabindex="0"><span><svg class="arrow"><use xlink:href="theme/img/sprite.svg#arrow"></use></svg></span></div>',
+        nextArrow: '<div data-role="none" class="slick-next" aria-label="Next" tabindex="0"><span><svg class="arrow"><use xlink:href="theme/img/sprite.svg#arrow"></use></svg></span></div>'
     });
 
     jQuery(function($){
